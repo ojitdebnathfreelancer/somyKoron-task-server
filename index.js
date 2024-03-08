c83c924617fe5020f7782e03051510dc5cf01793
unknown <ojitdebnathfreelancer.com>
2024-03-09 01:36:11 +0600

    full server done

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = 5000;
+import Replicate from "replicate";
+import "dotenv/config";
+
+app.use(cors());
+app.use(express.json());
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running for chat boat");
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const body = req.body;
+    const output = await replicate.run(process.env.MODEL, {
+      input: body,
+    });
+    res.send(output);
+  } catch (error) {
+    res.send("Something went wrong");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
